b58f0c5ab1810e056d6844b6549516a758437e58
Blessed Adodo <collzbadodo@gmail.com>
2022-10-29 19:03:46 -0700

    modified src-sw.js by adding configured workbox plugins for a service worker and manifest file

--- develop/client/webpack.config.js
+++ develop/client/webpack.config.js
@@ -3,8 +3,8 @@ const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
 
-// TODO: Add and configure workbox plugins for a service worker and manifest file.
-// TODO: Add CSS loaders and babel to webpack.
+//  Add and configure workbox plugins for a service worker and manifest file.
+//  Add CSS loaders and babel to webpack.
 
 module.exports = () => {
   return {
@@ -18,6 +18,15 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
+      new HtmlWebpackPlugin({
+        template: "./index.html",
+        title: "TODOs List",
+      }),
+      // Injecting custom service worker
+      new InjectManifest({
+        swSrc: "./src-sw.js",
+        swDest: "./src-sw.js",
+      }),
       
     ],
 
