45abcde8551fcc53435b55b8acc54c0c8f8b213e
Blessed Adodo <collzbadodo@gmail.com>
2022-10-30 16:54:09 -0700

    modified:   ../client/src-sw.js         modified:   ../client/webpack.config.js         modified:   ../package.json         modified:   routes/htmlRoutes.js         modified:   server.js

--- develop/client/src-sw.js
+++ develop/client/src-sw.js
@@ -38,12 +38,13 @@ const matchCallback = ({ request }) => {
   );
 };
 registerRoute(
-    // Here we define the callback function that will filter the requests we want to cache (in this case, JS and CSS files)
-  matchCallback,
+  // Here we define the callback function that will filter the requests we want to cache (in this case, JS and CSS files)
+  ({ request }) => ['style', 'script', 'worker'].includes(request.destination),
   new StaleWhileRevalidate({
-    cacheName,
+    // Name of the cache storage.
+    cacheName: 'asset-cache',
     plugins: [
-            // This plugin will cache responses with these headers to a maximum-age of 30 days
+      // This plugin will cache responses with these headers to a maximum-age of 30 days
       new CacheableResponsePlugin({
         statuses: [0, 200],
       }),
--- develop/client/webpack.config.js
+++ develop/client/webpack.config.js
@@ -3,72 +3,74 @@ const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
 
-//  Add and configure workbox plugins for a service worker and manifest file.
-//  Add CSS loaders and babel to webpack.
+// Add and configure workbox plugins for a service worker and manifest file.
+// Add CSS loaders and babel to webpack.
 
 module.exports = () => {
   return {
-    mode: 'development',
+    mode: "development",
     entry: {
-      main: './src/js/index.js',
-      install: './src/js/install.js'
+      main: "./src/js/index.js",
+      install: "./src/js/install.js",
     },
     output: {
-      filename: '[name].bundle.js',
-      path: path.resolve(__dirname, 'dist'),
+      filename: "[name].bundle.js",
+      path: path.resolve(__dirname, "dist"),
     },
     plugins: [
       new HtmlWebpackPlugin({
         template: "./index.html",
-        title: "text editor",
+        title: "TODOs List",
       }),
-      // Injecting custom service worker
-      new InjectManifest({
-        swSrc: "./src-sw.js",
-        swDest: "./src-sw.js",
-      }),
-      new WebpackPwaManifest({
-       //  Created a manifest.json:
-       fingerprints: false,
-       inject: true,
-       name: "My Progressive Web App",
-       short_name: "MyPWA",
-       description: "My awesome Progressive Web App!",
-       background_color: "#ffffff",
-       crossorigin: "use-credentials", 
-       start_url: "/",
-       publicPath: "/",
-       icons: [
-         {
-           src: path.resolve("src/images/logo.png"),
-           sizes: [96, 128, 192, 256, 384, 512], 
-           destination: path.join("assets/", "icons"),
-         },
-       ],
-     }),
-   ],
 
-   module: {
-     rules: [
-       {
-         test: /\.css$/i,
-         use: ["style-loader", "css-loader"],
-       },
-       {
-         test: /\.m?js$/,
-         exclude: /node_modules/,
-         use: {
-           loader: "babel-loader",
-           options: {
-             presets: ["@babel/preset-env"],
-             plugins: [
-               "@babel/plugin-proposal-object-rest-spread",
-               "@babel/transform-runtime",
-             ],
-           },
-         },
-       },
-     ],
-   },
- };
+      // Injects our custom service worker
+      new InjectManifest({
+        swSrc: "./src-sw.js", 
+        swDest: "src-sw.js",
+      }),
+
+      new WebpackPwaManifest({
+        // Create a manifest.json:
+        fingerprints: false,
+        inject: true,
+        name: "My Progressive Web App",
+        short_name: "MyPWA",
+        description: "My awesome Progressive Web App!",
+        background_color: "#ffffff",
+        crossorigin: "use-credentials",
+        start_url: "/",
+        publicPath: "./",
+        icons: [
+          {
+            src: path.resolve("src/images/logo.png"),
+            sizes: [96, 128, 192, 256, 384, 512], 
+            destination: path.join("assets/", "icons"),
+          },
+        ],
+      }),
+    ],
+
+    module: {
+      rules: [
+        {
+          test: /\.css$/i,
+          use: ["style-loader", "css-loader"],
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader",
+            options: {
+              presets: ["@babel/preset-env"],
+              plugins: [
+                "@babel/plugin-proposal-object-rest-spread",
+                "@babel/transform-runtime",
+              ],
+            },
+          },
+        },
+      ],
+    },
+  };
 };
