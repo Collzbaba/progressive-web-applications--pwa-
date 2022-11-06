1c060b73cb21fc747865d23ae7e25edd6b406819
Blessed Adodo <collzbadodo@gmail.com>
2022-11-06 10:12:20 -0800

    modified:   client/webpack.config.js

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,76 +1,49 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const WebpackPwaManifest = require('webpack-pwa-manifest');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 const path = require('path');
-const { InjectManifest } = require('workbox-webpack-plugin');
 
-// Add and configure workbox plugins for a service worker and manifest file.
-// Add CSS loaders and babel to webpack.
+module.exports = {
+  mode: 'development',
+  entry: './src/js/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
 
-module.exports = () => {
-  return {
-    mode: "development",
-    entry: {
-      main: "./src/js/index.js",
-      install: "./src/js/install.js",
-    },
-    output: {
-      filename: "[name].bundle.js",
-      path: path.resolve(__dirname, "dist"),
-    },
-    plugins: [
-      new HtmlWebpackPlugin({
-        template: "./index.html",
-        title: "TODOs List",
-      }),
-
-      // Injects our custom service worker
-      new InjectManifest({
-        swSrc: "./src-sw.js", 
-        swDest: "src-sw.js",
-      }),
-
-      new WebpackPwaManifest({
-        // TODO: Create a manifest.json:
-        fingerprints: false,
-        inject: true,
-        name: "My Progressive Web App",
-        short_name: "MyPWA",
-        description: "My awesome Progressive Web App!",
-        background_color: "#ffffff",
-        crossorigin: "use-credentials", 
-        start_url: "/",
-        publicPath: "./",
-        icons: [
-          {
-            src: path.resolve("src/images/logo.png"),
-            sizes: [96, 128, 192, 256, 384, 512], 
-            destination: path.join("assets/", "icons"),
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './index.html',
+      title: 'Webpack Plugin',
+    }),
+    new MiniCssExtractPlugin(),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
           },
-        ],
-      }),
+        },
+      },
     ],
-
-    module: {
-      rules: [
-        {
-          test: /\.css$/i,
-          use: ["style-loader", "css-loader"],
-        },
-        {
-          test: /\.m?js$/,
-          exclude: /node_modules/,
-          use: {
-            loader: "babel-loader",
-            options: {
-              presets: ["@babel/preset-env"],
-              plugins: [
-                "@babel/plugin-proposal-object-rest-spread",
-                "@babel/transform-runtime",
-              ],
-            },
-          },
-        },
+  },
+   optimization: {
+      minimizer: [
+        new CssMinimizerPlugin()
       ],
-    },
-  };
+      minimize: true
+    }
 };
